1722fafea60bb4a235b0cde1b9238559fa6aee97
Jeremy Moore <moore.jere@gmail.com>
2020-03-25 16:06:08 +1100

    RC5

--- modules/au.com.trgtd.tr.cal/src/au/com/trgtd/tr/cal/ctlr/DayListCtlr.java
+++ modules/au.com.trgtd.tr.cal/src/au/com/trgtd/tr/cal/ctlr/DayListCtlr.java
@@ -10,7 +10,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
