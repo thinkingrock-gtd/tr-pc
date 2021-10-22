b20cc3dbdade710f9c7e97ceec77803912d78417
degrown <91336981+degrown@users.noreply.github.com>
2021-10-22 21:05:08 +0200

    FIX: substituting boilerplate notice from CDDL to GPL v3
    As per GPL v3, we added the suggested notice at the top of all the sources files, substituting the CDDL one

--- modules/au.com.trgtd.tr.email/src/au/com/trgtd/tr/email/EmailUtils.java
+++ modules/au.com.trgtd.tr.email/src/au/com/trgtd/tr/email/EmailUtils.java
@@ -1,21 +1,19 @@
 /*
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can get a copy of the License at http://www.thinkingrock.com.au/cddl.html
- * or http://www.thinkingrock.com.au/cddl.txt.
- *
- * When distributing Covered Code, include this CDDL Header Notice in each file
- * and include the License file at http://www.thinkingrock.com.au/cddl.txt.
- * If applicable, add the following below the CDDL Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyright [year] [name of copyright owner]"
- *
- * The Original Software is ThinkingRock. The Initial Developer of the Original
- * Software is Avente Pty Ltd, Australia.
- *
- * Portions Copyright 2006-2007 Avente Pty Ltd. All Rights Reserved.
+ * ThinkingRock, a project management tool for Personal Computers. 
+ * Copyright (C) 2006-2007 Avente Pty Ltd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package au.com.trgtd.tr.email;
 
