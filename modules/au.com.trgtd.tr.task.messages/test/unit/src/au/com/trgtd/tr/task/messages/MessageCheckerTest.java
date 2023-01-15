794df894e1c16f39413ac88f518a80bace586fe6
Urs Joss <urs.joss@gmx.ch>
2023-01-15 11:44:33 +0100

    don't run the MessageCheckerTest by default. Intended for ad-hoc tests

--- modules/au.com.trgtd.tr.task.messages/test/unit/src/au/com/trgtd/tr/task/messages/MessageCheckerTest.java
+++ modules/au.com.trgtd.tr.task.messages/test/unit/src/au/com/trgtd/tr/task/messages/MessageCheckerTest.java
@@ -3,8 +3,13 @@ package au.com.trgtd.tr.task.messages;
 import au.com.trgtd.tr.task.messages.MessageParser.Message;
 import java.util.List;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 import org.junit.Test;
 
+/**
+ * Only run ad-hoc. Once the messages have been fetched, the test will fail.
+ */
+@Ignore
 public class MessageCheckerTest {
 
     final MessageChecker checker = new MessageChecker();
