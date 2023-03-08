e70e7b035df5c47e40c9a9638051951f0589d3fc
Urs Joss <urs.joss@gmx.ch>
2023-03-08 17:54:12 +0100

    refactor: MatcherEditorSearcher implementations (#117)

--- modules/au.com.trgtd.tr.view.reference/src/au/com/trgtd/tr/view/reference/filters/MatcherEditorSearchText.java
+++ modules/au.com.trgtd.tr.view.reference/src/au/com/trgtd/tr/view/reference/filters/MatcherEditorSearchText.java
@@ -1,17 +1,17 @@
 /*
- * ThinkingRock, a project management tool for Personal Computers. 
+ * ThinkingRock, a project management tool for Personal Computers.
  * Copyright (C) 2006 Avente Pty Ltd
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,14 +19,14 @@ package au.com.trgtd.tr.view.reference.filters;
 
 import ca.odell.glazedlists.matchers.Matcher;
 import java.awt.Component;
+import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.Serializable;
+import javax.swing.JTextField;
 import org.openide.util.NbBundle;
 import tr.model.information.Information;
-import java.awt.event.FocusAdapter;
-import java.awt.event.KeyAdapter;
-import javax.swing.JTextField;
 
 /**
  * MatcherEditor the matches information references for a search string.
@@ -34,10 +34,9 @@ import javax.swing.JTextField;
  * @author Jeremy Moore
  */
 public class MatcherEditorSearchText extends MatcherEditorBase {
-    
+
     private final JTextField searchText;
-    
-    /** Constructs a new instance. */
+
     public MatcherEditorSearchText() {
         searchText = new JTextField();
         searchText.addFocusListener(new FocusAdapter() {
@@ -48,7 +47,7 @@ public class MatcherEditorSearchText extends MatcherEditorBase {
         });
         searchText.addKeyListener(new KeyAdapter() {
             @Override
-            public void keyPressed(KeyEvent e) {
+            public void keyPressed(final KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     e.consume();
                     search();
@@ -56,57 +55,57 @@ public class MatcherEditorSearchText extends MatcherEditorBase {
             }
         });
     }
-    
+
+    @Override
     public Component getComponent() {
         return searchText;
     }
-    
+
     public void search() {
-        String string = searchText.getText();
-        if (string == null || string.trim().length() == 0) {
+        final String string = searchText.getText();
+        if (string == null || string.trim().isEmpty()) {
             fireMatchAll();
         } else {
             fireChanged(new SearchMatcher(string));
         }
     }
-    
+
+    @Override
     public String getLabel() {
         return NbBundle.getMessage(getClass(), "filter-search");
     }
-    
+
+    @Override
     public Serializable getSerializable() {
-        return (Serializable)searchText.getText();
+        return (Serializable) searchText.getText();
     }
-    
-    public void setSerializable(Serializable serializable) {
-        searchText.setText((String)serializable);
+
+    @Override
+    public void setSerializable(final Serializable serializable) {
+        searchText.setText((String) serializable);
     }
-    
+
     private static class SearchMatcher implements Matcher<Information> {
-        
+
         private final String search;
-        
+
         public SearchMatcher() {
             this.search = null;
         }
-        
-        public SearchMatcher(String search) {
+
+        public SearchMatcher(final String search) {
             this.search = search.toLowerCase();
         }
-        
-        public boolean matches(Information info) {
-            if (info.getDescription().toLowerCase().contains(search)) {
-                return true;
-            }
-            return false;
+
+        @Override
+        public boolean matches(final Information info) {
+            return info.getDescription().toLowerCase().contains(search);
         }
     }
-    
+
     @Override
     public void reset() {
         searchText.setText("");
         search();
     }
-   
 }
-
