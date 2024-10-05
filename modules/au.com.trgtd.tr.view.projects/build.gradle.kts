935ebe7843494d7c6218798581939a405ebc4ce8
Urs Joss <urs.joss@gmx.ch>
2024-10-05 19:11:59 +0200

    au.com.trgtd.tr.view.filters

---
+++ modules/au.com.trgtd.tr.view.projects/build.gradle.kts
@@ -0,0 +1,26 @@
+description = "TR-View-Projects"
+
+plugins {
+    id("java-library")
+    id("au.com.trgtd.migration.antpathsourceset")
+}
+
+dependencies {
+    implementation(project(":au.com.trgtd.tr.data"))
+    implementation(project(":au.com.trgtd.tr.extract"))
+    implementation(project(":au.com.trgtd.tr.prefs.projects"))
+    implementation(project(":au.com.trgtd.tr.report.project.detail"))
+    implementation(project(":au.com.trgtd.tr.report.project.outline"))
+    implementation(project(":au.com.trgtd.tr.swing"))
+    implementation(project(":au.com.trgtd.tr.view"))
+    implementation(project(":au.com.trgtd.tr.view.actn"))
+    implementation(project(":au.com.trgtd.tr.view.contexts"))
+    implementation(project(":au.com.trgtd.tr.view.goals"))
+    implementation(project(":au.com.trgtd.tr.view.project"))
+    implementation(project(":au.com.trgtd.tr.view.topics"))
+    implementation(project(":tr.extract.reports"))
+
+    implementation(libs.itext)
+    implementation(libs.jasperreports)
+    implementation(libs.netbeans.api.jdesktop.layout)
+}
